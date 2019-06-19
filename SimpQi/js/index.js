0ff4d00f21c874155e0f8737b7347d308729b1ed
Raffael Rehberger <rarepublic@protonmail.com>
2019-06-19 05:07:16 +0200

    manually inject backendService and persistence into LoginController

--- SimpQi/js/index.js
+++ SimpQi/js/index.js
@@ -1,7 +1,19 @@
+function renderSimpQiUnsupportedError(reason) {
+    return "Excuse me, but it seems that your browser is a little out of date. " + reason
+}
+
 $(document).ready(function() {
-    var loginController = new LoginController(function(username){
+    var persistenceService = new PersistenceService();
+    if(!persistenceService.isClientSupportingLocalStorage() 
+        || !persistenceService.isClientSupportingsSessionStorage()) {
+            alert(renderSimpQiUnsupportedError("Browser does not support Session-/LocalStorage."));
+    }
+
+    var backendService = new BackendServiceFactory("localhost", 1337, true).create();
+
+    var loginController = new LoginController(backendService, persistenceService, function(username){
        alert("User " + username + " ist jetzt angemeledet");
     });
+
     loginController.display();
-    loginController.initialize();
 });
