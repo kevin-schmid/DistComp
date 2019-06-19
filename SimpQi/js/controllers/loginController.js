73a276248ad4ed7440ad2da4873e66908740f906
Raffael Rehberger <rarepublic@protonmail.com>
2019-06-19 05:06:29 +0200

    use browser-storage to remember username, rework ui, use backendService

--- SimpQi/js/controllers/loginController.js
+++ SimpQi/js/controllers/loginController.js
@@ -1,29 +1,63 @@
 class LoginController {
-    constructor(nextStepCallback) {
-        this.nextStepCallback = nextStepCallback;
-    }
+    constructor(backendService, persistenceService, loginSuccessCallback) {
+        if(backendService === undefined)
+            throw Error("BackendService may not be undefined");
+        if(persistenceService === undefined) 
+            throw Error("PersistenceService may not be undefined");
+        if(loginSuccessCallback === undefined)
+            throw Error("LoginSuccess-Callback may not be undefined");
 
-    initialize() {
-        var self = this;
-        $('.js-username-input').keyup(function(ev){
-           if(ev.keyCode === 13) { // keyCode 13 == Enter
-               self.tryLogin();
-           }
-        });
-        $('.js-login-button').on("click", function() {
-            self.tryLogin();
-        });
+        this.backendService = backendService;
+        this.persistenceService = persistenceService;
+        this.loginSuccessCallback = loginSuccessCallback;
     }
 
     display() {
         renderLogin();
+        this.initialize();
+    }
+
+    initialize() {
+        var self = this;
+
+        /* Load Username from last Session, to help user save time */
+        var lastUsername = this.persistenceService
+            .loadFromLocalStorage('lastUsername');
+
+        if(!$.isEmptyObject(lastUsername)) {
+            $('.js-username-input').val(lastUsername);
+        }
+
+        /* User presses Enter on Input-Textbox */
+        $('.js-username-input').keyup(function(ev){
+           if(ev.keyCode === 13) self.tryLogin();
+        });
+
+        /* User clicks Login-Button */
+        $('.js-login-button').on("click", function() { 
+            self.tryLogin();
+        });
     }
 
     tryLogin() {
         var username = $('.js-username-input').val();
-        if(username !== "herbert") {
-            alert("Username is already taken");
+        var loginResult = this.backendService.tryLogin(username);
+        if(loginResult.success === false) {
+            this.flashLoginError(loginResult.message);
+            return;
         }
-        this.nextStepCallback(username);
+
+        this.persistenceService
+            .persistToLocalStorage('lastUsername', username);
+
+        this.loginSuccessCallback(username);
+    }
+
+    flashLoginError(message) {
+        $('.js-error-message')
+            .html(message)
+            .show()
+            .fadeOut(5000);
+
     }
 }
