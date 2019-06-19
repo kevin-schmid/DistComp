29fb55a44603980b47071d3faf4392dc4e8cde00
Raffael Rehberger <rarepublic@protonmail.com>
2019-06-19 19:10:39 +0200

    adapt layout two centered card for waiting view

---
+++ SimpQi/js/views/gameView.js
@@ -0,0 +1,36 @@
+const PlayerCardTemplate = `
+<div class="ui centered link cards">
+    <div class="card">
+        <div class="image">
+            <img class="js-card-image" src="">
+        </div>
+        <div class="content">
+            <div class="header js-card-name"></div>
+                <div class="meta">
+                    <a>SimpQi-Player</a>
+                </div>
+            <div class="description">
+                This is you for the next game! Please wait while we find players.
+            </div>
+        </div>
+        <div class="extra content">
+            <span class="right floated">
+                2019
+            </span>
+            <span>
+                <i class="user icon"></i>
+                Austria
+            </span>
+        </div>
+    </div>
+</div>
+`;
+
+function renderPlayerCardWaiting(playerName, playerImageUrl) {
+    $(document).attr("title", "SimpQi | Waiting");
+    var template = $(PlayerCardTemplate);
+    template.find('.js-card-name').text(playerName);
+    template.find('.js-card-image').attr('src', playerImageUrl);
+
+    $('.js-centered-body').html(template);
+}
