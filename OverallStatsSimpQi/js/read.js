be336326e46eda012ceee0519e0d3bea531d36ec
Martin Walzl <martin.walzl@edu.fh-joanneum.at>
2019-07-04 18:33:46 +0200

    some mongo stuff

--- OverallStatsSimpQi/js/read.js
+++ OverallStatsSimpQi/js/read.js
@@ -2,7 +2,9 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 var mongoose = require('mongoose');
-
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override')
+var router = express.Router();
 var app = express();
 
 // all environments
@@ -10,17 +12,29 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
+app.use(bodyParser());
+app.use(methodOverride());
+app.use(router);
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://admin:<password>@cluster0-u60tn.gcp.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
 
 mongoose.connect('mongodb://localhost/Company');
 
 var Schema = new mongoose.Schema({
     _id    : String,
-    name: String,
-    age   : Number
+    user: String,
+    games_won   : Number,
+    games_lost: Number,
+    ranking: Number
 });
 
 var user = mongoose.model('emp', Schema);
